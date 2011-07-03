2e0171dc103e8c77b7dabacbac16af506b220163
Sleaker <sleaker@gmail.com>
2011-07-03 14:49:28 -0700

    removed unecessary sendmessage runnable

--- src/net/milkbowl/combatevents/Utility.java
+++ src/net/milkbowl/combatevents/Utility.java
@@ -1,7 +1,6 @@
 package net.milkbowl.combatevents;
 
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 
 public class Utility {
 
@@ -10,26 +9,5 @@ public class Utility {
 		double yDistSq = Math.pow(loc1.getY() - loc2.getY(), 2);
 		double zDistSq = Math.pow(loc1.getZ() - loc2.getZ(), 2);
 		return Math.sqrt(xDistSq + yDistSq + zDistSq);
-	}
-
-	public class SendMessage implements Runnable {
-		String player;
-		String message;
-		CombatEventsCore plugin;
-
-		SendMessage (String player, String message, CombatEventsCore plugin) {
-			this.player = player;
-			this.message = player;
-			this.plugin = plugin;
-		}
-
-		@Override
-		public void run() {
-			Player player = plugin.getServer().getPlayer(this.player);
-			if (player == null)
-				return;
-			else
-				player.sendMessage(message);
-		}
 	}
 }
